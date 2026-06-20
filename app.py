5e37c52474d2c9589b393709b815c9e5ac77b069
Yogiraj Bhilare <yogirajbhilare1107@gmail.com>
2026-06-20 20:53:56 +0530

    Cahnges in app.py

--- app.py
+++ app.py
@@ -21,7 +21,11 @@ PhoneService = st.selectbox("Phone Service", ["Yes", "No"])
 MultipleLines = st.selectbox("Multiple Lines", ["Yes", "No", "No phone service"])
 InternetService = st.selectbox("Internet Service", ["DSL", "Fiber optic", "No"])
 OnlineSecurity = st.selectbox("Online Security", ["Yes", "No", "No internet service"])
+OnlineBackup = st.selectbox("Online Backup", ["Yes", "No", "No internet service"])
+DeviceProtection = st.selectbox("Device Protection", ["Yes", "No", "No internet service"])
 TechSupport = st.selectbox("Tech Support", ["Yes", "No", "No internet service"])
+StreamingTV = st.selectbox("Streaming TV", ["Yes", "No", "No internet service"])
+StreamingMovies = st.selectbox("Streaming Movies", ["Yes", "No", "No internet service"])
 Contract = st.selectbox("Contract Type", ["Month-to-month", "One year", "Two year"])
 PaperlessBilling = st.selectbox("Paperless Billing", ["Yes", "No"])
 PaymentMethod = st.selectbox(
@@ -44,7 +48,11 @@ if st.button("Predict Churn"):
         "MultipleLines": MultipleLines,
         "InternetService": InternetService,
         "OnlineSecurity": OnlineSecurity,
+        "OnlineBackup": OnlineBackup,
+        "DeviceProtection": DeviceProtection,
         "TechSupport": TechSupport,
+        "StreamingTV": StreamingTV,
+        "StreamingMovies": StreamingMovies,
         "Contract": Contract,
         "PaperlessBilling": PaperlessBilling,
         "PaymentMethod": PaymentMethod,
